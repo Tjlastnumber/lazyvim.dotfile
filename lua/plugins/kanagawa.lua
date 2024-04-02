b5e6409f06010a6d0c8916fe5918638ae4f76cda
shao xuehai <shaoxh@chnsys.com.cn>
2024-04-02 10:49:21 +0800

    update: 添加窗口透明度

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -2,6 +2,8 @@
 return {
   {
     "rebelot/kanagawa.nvim",
+    priority = 1000,
+    lazy = true,
     enabled = true,
     opts = {
       compile = false, -- enable compiling the colorscheme
@@ -11,7 +13,7 @@ return {
       keywordStyle = { italic = true },
       statementStyle = { bold = true },
       typeStyle = {},
-      transparent = false, -- do not set background color
+      transparent = true, -- do not set background color
       dimInactive = false, -- dim inactive window `:h hl-NormalNC`
       terminalColors = true, -- define vim.g.terminal_color_{0,17}
       colors = {
